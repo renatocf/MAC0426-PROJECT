d092e84bfae78ceb1662f265fc7e8f6ab35fad17
Renato Cordeiro Ferreira <renatocf@users.noreply.github.com>
2014-05-15 13:53:54 -0300

    Update apagaTudo.sql

--- apagaTudo.sql
+++ apagaTudo.sql
@@ -1,11 +1,11 @@
-USE renatocf
-;DROP TABLE gamer
-;DROP TABLE team 
-;DROP TABLE game
-;DROP TABLE distributor
-;DROP TABLE challenge
-;DROP TABLE rel_distribute
-;DROP TABLE rel_owns
-;DROP TABLE rel_participate
-;DROP TABLE rel_play
-;DROP TABLE rel_dispute;
+USE renatocf;
+DROP TABLE gamer;
+DROP TABLE team;
+DROP TABLE game;
+DROP TABLE distributor;
+DROP TABLE challenge;
+DROP TABLE rel_distribute;
+DROP TABLE rel_owns;
+DROP TABLE rel_participate;
+DROP TABLE rel_play;
+DROP TABLE rel_dispute;
